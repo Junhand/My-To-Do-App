30ce725f1731af40d074129b1572a636b1ef3dfc
河越　淳 <jun1493907@gmail.com>
2017-11-29 08:47:09 +0900

    to do

---
+++ Core ToDo for Qiita/Core ToDo for Qiita/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  Core ToDo for Qiita
+//
+//  Created by JJJ on 2017/11/25.
+//  Copyright © 2017年 jun. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
